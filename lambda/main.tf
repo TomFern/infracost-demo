8833633aa7548ba883b984494271b4832ac0b2c2
tomas f <46322567+TomFern@users.noreply.github.com>
2024-04-22 15:26:42 -0300

    lambda

--- lambda/main.tf
+++ lambda/main.tf
@@ -8,6 +8,6 @@ resource "aws_lambda_function" "my_lambda" {
   function_name = "my_lambda"
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
   handler       = "exports.test"
-  runtime       = "nodejs12.x"
+  runtime       = "nodejs20.x"
   memory_size   = 1024
 }
